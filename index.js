f65359d9ee0fa98c7dd7065a873b233c3b5dfef5
arpan  das <dasarpan471@gmail.com>
2021-08-24 11:09:35 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const handlebars = require("handlebars")
+var pdf = require("html-pdf");
+
+var convert = (doc, opt) => {
+    return new Promise((resolve, reject) => {
+        if (!doc || !doc.html || !doc.data) {
+            reject(new Error("some, or all , options are missing."))
+        }
+        var HTML = handlebars.compile(doc.html)(doc.data);
+        var PromiseFor = pdf.create(HTML, opt)
+
+        switch (doc.type) {
+            case "buffer":
+                PromiseFor.toBuffer((err, res) => {
+                    if (!err) resolve(res);
+                    else reject(err);
+                });
+                breask;
+            case "stream":
+                PromiseFor.toStream((err, res) => {
+                    if (!err) resolve(res);
+                    else reject(err);
+                });
+                break;
+            default:
+                PromiseFor.toFile(document.path, (err, res) => {
+                    if (!err) resolve(res);
+                    else reject(err);
+                });
+                break;
+        }
+    })
+};
+
+module.exports.convert = convert;
